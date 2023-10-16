8b9bc43ccf0b40159e151221515a04afdd559d5c
Illia Belikau <ilasemcenkov1@gmail.com>
2023-10-15 23:13:35 -1100

    first commit

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,35 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
+public class User {
+    private static final String SEQ_NAME = "user_seq";
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator =  SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME)
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private boolean archive;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @OneToOne(cascade = CascadeType.REMOVE)
+    private Bucket bucket;
+
+
+
+
+}
